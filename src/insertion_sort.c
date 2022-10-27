406cf3d4cc64f1d6be2e6097361d6889903a4b7e
MikkelHojbjerg <MikkelHojbjerg1@gmail.com>
2022-10-27 22:14:57 +0200

    opgave 2 færdig

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -14,15 +14,15 @@ void sort(linked_list *llPtr){
 	node_t *curr = llPtr->head->next;
 	node_t *prev = llPtr->head;
 
+	printf("\nNy liste:\n");
+
 	//Kører hele listen igennem
 	while(prev->next != NULL){
-
-		printf("1\n");
 
 		//Tjekker om curr er mindre end prev, hvis ja skal curr flyttes
 		if(curr->data < prev->data){
 
-			printf("2\n");
+			printf("curr: %d, prev: %d\n", curr->data, prev->data);
 
 			//Temporary prev bliver brugt til at vide hvor curr skal flyttes hen
 			node_t *temp_prev = llPtr->head;
@@ -30,17 +30,14 @@ void sort(linked_list *llPtr){
 			//Looper indtil temp_prev->next er NULL
 			while(temp_prev->next != NULL){
 
-				printf("3\n");
-
 				//Tjekker om curr er mindre end head
 				if(curr->data < llPtr->head->data){
 
-					printf("4\n");
-
-					prev = curr->next;
+					prev->next = curr->next;
 					curr->next = llPtr->head;
 					llPtr->head = curr;
 					curr = prev->next;
+
 					//Når curr er flyttet og er på sin rette plads, er der ikke nogle grund til at køre resten af loopet
 					break;
 				}
@@ -48,17 +45,11 @@ void sort(linked_list *llPtr){
 				//Hvis tjekker om det næste element i listen er mindre end curr
 				if(curr->data > temp_prev->next->data){
 
-					printf("5\n");
-
 					//Hvis ja rykker temp_prev et element op i listen
 					temp_prev = temp_prev->next;
 				}else{
 
-					printf("6\n");
-
 					if(curr->next !=NULL){
-
-						printf("!=NULL\n");
 
 						//Hvis det næste element er større end curr, rykkes curr på dens rette plads
 						prev->next = curr->next;
@@ -70,14 +61,9 @@ void sort(linked_list *llPtr){
 						break;
 					}else{
 
-						printf("NULL\n");
-
 						prev->next = NULL;
 						curr->next = temp_prev->next;
 						temp_prev->next = curr;
-
-						printf("prev->next: %d\n", prev->next);
-
 						break;
 					}
 
@@ -86,6 +72,9 @@ void sort(linked_list *llPtr){
 			}
 
 		}else{
+
+			printf("curr: %d, prev: %d\n", curr->data, prev->data);
+
 			//curr og prev rykker et element frem i listen
 			curr = curr->next;
 			prev = prev->next;
