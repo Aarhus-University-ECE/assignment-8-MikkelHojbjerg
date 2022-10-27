e4797ed3139429a6294fa7c330ef606ef9d4aca7
MikkelHojbjerg <MikkelHojbjerg1@gmail.com>
2022-10-27 16:40:05 +0200

    Opg 2 næsten færdig

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -1,9 +1,74 @@
 #include "insertion_sort.h"
 #include "linked_list.h"
+#include <stdio.h>
 
 
+void sort(linked_list *llPtr){
 
-void sort(linked_list *llPtr)
-{
- // Add your sort function here
+	//Tjekker om listen er tom
+	if(llPtr->head == NULL){
+		printf("Listen er tom");
+		return;
+	}
+
+	node_t *curr = llPtr->head->next;
+	node_t *prev = llPtr->head;
+
+	printf("head = %d\n", llPtr->head->data);
+
+	//Kører hele listen igennem
+	while(curr != NULL){
+
+		//Tjekker om curr er mindre end prev, hvis ja skal curr flyttes
+		if(curr->data < prev->data){
+
+			//Temporary prev bliver brugt til at vide hvor curr skal flyttes hen
+			node_t *temp_prev = llPtr->head;
+
+			/*Curr skal have en ny plads mellem intervallet head(temp_prev) og prev.
+			Derfor hedder loopet temp_prev != prev.
+			*/
+
+			while(temp_prev->next != NULL){
+
+				//Tjekker om curr er mindre end head
+				if(curr->data < llPtr->head->data){
+
+					prev = curr->next;
+					curr->next = llPtr->head;
+					llPtr->head = curr;
+					curr = prev->next;
+					//Når curr er flyttet og er på sin rette plads, er der ikke nogle grund til at køre resten af loopet
+					break;
+				}
+
+				//Hvis tjekker om det næste element i listen er mindre end curr
+				if(curr->data > temp_prev->next->data){
+					//Hvis ja rykker temp_prev et element op i listen
+					temp_prev = temp_prev->next;
+				}else{
+					//Hvis det næste element er større end curr, rykkes curr på dens rette plads
+
+					prev->next = curr->next;
+
+					curr->next = temp_prev->next;
+
+					temp_prev->next = curr;
+
+					curr = prev->next;
+
+					break;
+				}
+
+			}
+
+
+		}else{
+			curr = curr->next;
+			prev = prev->next;
+
+		}
+
+	}
+
 }
